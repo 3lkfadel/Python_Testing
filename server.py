0d2adf21c213a8099e7991be34980b6fb0745f1d
3lkfadel <3lkfadel@gmail.com>
2024-09-30 08:01:02 +0000

    recherche solution

--- server.py
+++ server.py
@@ -4,14 +4,14 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 
 def loadClubs():
     with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
 
 
 def loadCompetitions():
     with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+        listOfCompetitions = json.load(comps)['competitions']
+        return listOfCompetitions
 
 
 app = Flask(__name__)
@@ -27,7 +27,11 @@ def index():
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    if club :
+        return render_template('welcome.html',club=club,competitions=competitions)
+    else:
+        flash("Email not found. Please try again.")
+        return redirect(url_for('index'))
 
 
 @app.route('/book/<competition>/<club>')
