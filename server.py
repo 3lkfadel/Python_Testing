2c4e681f5d307bd4e0ce0fa862a51cfbcc0f36e8
3lkfadel <3lkfadel@gmail.com>
2024-10-02 11:27:39 +0000

    limitation des places

--- server.py
+++ server.py
@@ -50,25 +50,33 @@ def book(competition,club):
 reservations = []
 
 @app.route('/purchasePlaces', methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
+    competition = next((c for c in competitions if c['name'] == request.form['competition']), None)
+    club = next((c for c in clubs if c['name'] == request.form['club']), None)
     placesRequired = int(request.form['places'])
-    
+
     # Vérification du nombre de places disponibles
     if placesRequired > int(competition['numberOfPlaces']):
         flash("Pas assez de places disponibles.")
         return redirect(url_for('book', competition=competition['name'], club=club['name']))
-    
+
+    # Vérification des points disponibles
+    if placesRequired > club['points']:
+        flash("Vous n'avez pas assez de points pour réserver ce nombre de places.")
+        return redirect(url_for('book', competition=competition['name'], club=club['name']))
+
+    # Réduire le nombre de places disponibles et les points du club
     competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
-    
+    club['points'] -= placesRequired  # Diminuer les points en fonction des places réservées
+
     # Ajouter la réservation à la liste des réservations
     reservations.append({
         'club': club['name'],
         'competition': competition['name'],
         'places': placesRequired
     })
-    
+
     flash('Réservation effectuée avec succès !')
     return render_template('welcome.html', club=club, competitions=competitions, reservations=reservations)
 
